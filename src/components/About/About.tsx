af16dc55633eeb8707a873afa91d48a0b1401b04
Ashish Thapa <heyashishthapa@gmail.com>
2024-11-18 17:22:15 +0545

    about changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -20,12 +20,12 @@ export function About() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
           <p>
-            Hi there! I'm Ashish, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, and React, I have the tools to bring any website vision to life.
+            Hi there! I'm Ashish, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, PHP, React JS, and Tailwind CSS, I have the tools to bring any website vision to life.
           </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p>
-            But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress and Shopify, making it easy for my clients to take control of their own websites and keep them up to date.</p>
+            But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress, making it easy for my clients to take control of their own websites and keep them up to date.</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
           <p>
