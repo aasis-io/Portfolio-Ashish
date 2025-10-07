4a4e3345ece25a6fa3fe2da38a2d9be16ce95098
Ashish Thapa <heyashishthapa@gmail.com>
2025-10-07 16:41:16 +0545

    fixes

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,29 +1,30 @@
-import { Container } from "./styles";
-import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import { Mail, Phone } from "lucide-react"; // import lucide icons
 import { Form } from "../Form/Form";
+import { Container } from "./styles";
 
-
-export function Contact(){
-
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <header>
         <h2>Contact</h2>
-        <p>Ready to get started on your project? </p>
+        <p>Ready to get started on your project?</p>
         <p>Contact me now for a Free consultation.</p>
       </header>
       <div className="contacts">
-        <div>
-        <a href="mailto:helloashishthapa@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+        <div className="contact-item">
+          <a href="mailto:helloashishthapa@gmail.com">
+            <Mail size={24} /> {/* Lucide icon */}
+          </a>
           <a href="mailto:helloashishthapa@gmail.com">helloashishthapa@gmail.com</a>
         </div>
-        <div>
-        <a href="tel:+9779840033688"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919630576848">(+977) 9840033688</a>
-        </div>  
+        <div className="contact-item">
+          <a href="tel:+9779840033688">
+            <Phone size={24} /> {/* Lucide icon */}
+          </a>
+          <a href="tel:+9779840033688">(+977) 9840033688</a>
+        </div>
       </div>
-      <Form></Form>
+      <Form />
     </Container>
-  )
+  );
 }
